961bcb1c93a3481813d52f0ee53adfd1b303b428
nosferati <artswanted@icloud.com>
2021-01-30 09:51:03 +0200

    14.3

--- Module - 14/quest3/main.cpp
+++ Module - 14/quest3/main.cpp
@@ -9,7 +9,8 @@ bool iValidatorEmail (std::string pKEmail){
         for (int j = 0; j < badSymbol.size(); j++){
             if (pKEmail[i] == badSymbol[j]) {
                 iCorrect = false;
-            }break;
+                break;
+            }
         }
         if (!iCorrect) break;
     }
@@ -32,7 +33,7 @@ int main() {
         if (correct) {
             std::cout << "Email is correct!" << std::endl;
         } else {
-            std::cout << "Email is\'nt correct!"<< std::endl;
+            std::cout << "Email isnt correct!"<< std::endl;
         }
 
         std::cout << "Enter 1 for try again or any number for exit: ";
