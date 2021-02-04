d39ed230a34d7603a208b13ca090fdc2757ec88f
nosferati <artswanted@icloud.com>
2021-02-04 20:47:52 +0200

    14.2

--- Module - 14/quest2/main.cpp
+++ Module - 14/quest2/main.cpp
@@ -8,54 +8,24 @@ int main() {
                                           {'|',' ',' ',' ','|'},
                                           {'|',' ',' ',' ','|'},
                                           {'-','-','-','-','-'}};
-    std::vector<std::vector<char>> xoFresh = xoVec;
-    char choice;
-    bool exit = false, reset = false;
-    int line, column;
-    do {
-        std::cout << "Write X or 0 for play or Q for exit or R for reset: " << std::endl;
-        do {
-            std::cin >> choice;
-            if (choice == 'X' || choice == '0') break;
-            else if (choice == 'Q'){
-                exit = true;
-                break;
-            } else if (choice == 'R') {
-                reset = true;
-                break;
-            } else {
-                std::cout << "Incorrect!" << std::endl;
-            }
-        } while (true);
 
-        if (exit) break;
-        else if (!reset) {
-            std::cout << "Write line number (1-3): " << std::endl;
-            do {
-                std::cin >> line;
-                if (line >= 1 && line <= 3) break;
-                else std::cout << "Incorrect!" << std::endl;
-            } while (true);
+    std::string iLine;
 
-            std::cout << "Write column in line (1-3): " << std::endl;
-            do {
-                std::cin >> column;
-                if (column >= 1 && column <= 3) break;
-                else std::cout << "Incorrect!" << std::endl;
-            } while (true);
-
-            xoVec[line][column] = choice;
+    std::cout << "You can use .(dot) O and X (template O.X)." << std::endl;
+    for (int l = 0; l < 3; l++){
+        std::cout << "Write line number "<< l+1 <<": " << std::endl;
+        std::cin >> iLine;
+        for (int k = 0; k < 3; k++){
+            xoVec[1+l][1+k] = iLine[k];
         }
+    }
 
-        if (reset) xoVec = xoFresh;
-
-        for (int i = 0; i < 5; i++){
-            for (int j = 0; j < 5; j++){
-                std::cout << xoVec[i][j];
-            }
-            std::cout << std::endl;
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j < 5; j++){
+            std::cout << xoVec[i][j];
         }
-    } while (true);
+        std::cout << std::endl;
+    }
 
     std::cout << "Have a nice day!" << std::endl;
     return 0;
