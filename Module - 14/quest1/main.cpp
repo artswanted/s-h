7f04037faaf5bd128842a7b25323dee470a8b160
nosferati <artswanted@icloud.com>
2021-02-04 20:28:35 +0200

    14.1
    Encode only Aa->Zz

--- Module - 14/quest1/main.cpp
+++ Module - 14/quest1/main.cpp
@@ -5,19 +5,29 @@ using namespace std;
 string encryptCaeser(string iMessage, int iFormula){
     string encMessage;
     char letter, solution;
-    cout << "+ or - ?" << endl;
+    cout << "White + or -" << std::endl; //here you can choice way
     do {
         cin >> solution;
         if (solution == '+') {
             for (int i = 0; i < iMessage.size(); i++){
-                letter = iMessage[i] + iFormula;
-                encMessage += letter;
+                if ((iMessage[i] > 'a' && iMessage[i] > 'z') ||
+                    (iMessage[i] > 'A' && iMessage[i] > 'Z')){
+                    letter = iMessage[i] + iFormula;
+                    encMessage += letter;
+                } else {
+                    encMessage += iMessage[i];
+                }
             }
             break;
         } else if (solution == '-'){
             for (int i = 0; i < iMessage.size(); i++){
-                letter = iMessage[i] - iFormula;
-                encMessage += letter;
+                if ((iMessage[i] > 'a' && iMessage[i] > 'z') ||
+                    (iMessage[i] > 'A' && iMessage[i] > 'Z')) {
+                    letter = iMessage[i] - iFormula;
+                    encMessage += letter;
+                } else {
+                    encMessage += iMessage[i];
+                }
             }
             break;
         } else cout << "Incorrect." << endl;
