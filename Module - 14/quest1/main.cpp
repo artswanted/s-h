1cd0acb956b65d3f1a9a13bc1eb44b8e06182e5e
nosferati <artswanted@icloud.com>
2021-02-14 11:13:05 +0200

    14.1

--- Module - 14/quest1/main.cpp
+++ Module - 14/quest1/main.cpp
@@ -3,33 +3,28 @@
 using namespace std;
 
 char iChangeSymbol(char iSymbol, int iFormula){
-    if ((iSymbol < 'A' && iSymbol > 'Z') && (iSymbol < 'a' && iSymbol > 'z')) {
-        cout << "bug 1";
-        return iSymbol;
-    }
-
-    if(iSymbol > 'A' && iSymbol < 'Z'){
+    if(iSymbol >= 'A' && iSymbol <= 'Z'){
         if (iSymbol + iFormula > 'Z' || iSymbol + iFormula < 'A' ){
             if (iSymbol + iFormula < 'A'){
-                return 'Z' - (iFormula - (iSymbol - 'A'));
+                return 'Z' - (iFormula - (iSymbol - 'A')+1);
             } else if (iSymbol + iFormula > 'Z'){
-                return 'A' + (iSymbol + iFormula - 'Z');
+                return 'A' + ((iSymbol + iFormula - 'Z')-1);
             }
         } else {
             return iSymbol + iFormula;
         }
-    } else if (iSymbol < 'a' && iSymbol > 'z'){
-        cout << "bug 2";
+    } else if (iSymbol >= 'a' && iSymbol <= 'z'){
         if (iSymbol + iFormula > 'z' || iSymbol + iFormula < 'a' ){
             if (iSymbol + iFormula < 'a'){
-                return 'z' - (iFormula - (iSymbol - 'a'));
-            } else if (iSymbol + iFormula > 'Z'){
-                return 'a' + (iSymbol + iFormula - 'z');
+                return 'z' - (iFormula - (iSymbol - 'a')+1);
+            } else if (iSymbol + iFormula > 'z'){
+                return 'a' + ((iSymbol + iFormula - 'z')-1);
             }
         } else {
-            cout << "bug 3";
             return iSymbol + iFormula;
         }
+    } else {
+        return iSymbol;
     }
 }
 
@@ -39,7 +34,6 @@ string encryptCaeser(string iMessage, int iFormula){
     for (int i = 0; i < iMessage.size(); i++) {
         temp = iChangeSymbol(iMessage[i], iFormula);
         encMessage += temp;
-        cout << iChangeSymbol(iMessage[i], iFormula);
     }
     return encMessage;
 }
