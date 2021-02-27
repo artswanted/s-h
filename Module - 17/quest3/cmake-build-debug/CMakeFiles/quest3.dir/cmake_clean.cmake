cfd8fa1ac5ad4612204dac2f07f9026801e9f85f
nosferati <artswanted@icloud.com>
2021-02-27 21:52:03 +0200

    14.3

---
+++ Module - 17/quest3/cmake-build-debug/CMakeFiles/quest3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quest3.dir/main.cpp.obj"
+  "quest3.exe"
+  "quest3.exe.manifest"
+  "quest3.lib"
+  "quest3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quest3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Module - 17/quest3/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int x = 10;
+    int y = 10;
+    int array [x][y];
+    for (int i=0; i < x; i++){
+        for (int j = 0; )
+    }
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
