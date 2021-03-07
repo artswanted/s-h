f7669261b0c719d7e5438262f48653a6b1cd84b9
artswanted <artswanted@icloud.com>
2021-03-07 17:18:36 +0200

    17.7

---
+++ Module - 17/quest7/cmake-build-debug/CMakeFiles/quest7.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quest7.dir/main.cpp.obj"
+  "quest7.exe"
+  "quest7.exe.manifest"
+  "quest7.lib"
+  "quest7.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quest7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
