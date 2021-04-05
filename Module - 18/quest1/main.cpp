a50f892d731621f6720e31f57cd63faf3949bd0d
artswanted <artswanted@icloud.com>
2021-04-05 21:53:16 +0300

    18.1

--- Module - 18/quest1/main.cpp
+++ Module - 18/quest1/main.cpp
@@ -1,6 +1,54 @@
 #include <iostream>
+#include <vector>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<int> vec = { 2, -2, 14, -32, 10, 2, 0, -1, 6, 17, -40, 22, 15, 10 };
+    int posI = 0;
+    int posJ = 0;
+    int curI = 0;
+    int curJ = 0;
+    int curSum = 0;
+    int max = vec[0];
+    bool newSum = false;
+    bool haveNotI = true;
+    for (int i = 0; i < vec.size() - 1; ++i)
+    {
+        if (newSum || vec[i] >= 0)
+        {
+            newSum = true;
+            if (curSum + vec[i] >= curSum)
+            {
+                if (haveNotI)
+                {
+                    curI = i;
+                    haveNotI = false;
+                }
+                curJ = i;
+                curSum += vec[i];
+            }
+            else{
+                if (curSum + vec[i] < 0)
+                {
+                    haveNotI = true;
+                    newSum = false;
+                    curSum = 0;
+                }
+                else
+                    curSum += vec[i];
+            }
+            if (curSum > max)
+            {
+                max = curSum;
+                posI = curI;
+                posJ = curJ;
+            }
+        }
+        else{
+            curSum = 0;
+        }
+    }
+    std::cout << "max = " << max << std::endl;
+    std::cout << "i = " << posI << std::endl;
+    std::cout << "j = " << posJ << std::endl;
     return 0;
 }
