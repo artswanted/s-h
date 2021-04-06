43701b91b116fe4578dd135ea99af3cee77b1916
artswanted <artswanted@icloud.com>
2021-04-06 09:43:42 +0300

    18.2

---
+++ Module - 18/quest2/cmake-build-debug/CMakeFiles/18_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "18_2.exe"
+  "18_2.exe.manifest"
+  "18_2.lib"
+  "18_2.pdb"
+  "CMakeFiles/18_2.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/18_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Module - 18/quest2/cmake-build-debug/cmake_install.cmake
+++ Module - 18/quest2/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: F:/skillbox-homework/Module - 18/18.2
+# Install script for directory: F:/skillbox-homework/Module - 18/quest2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "F:/skillbox-homework/Module - 18/18.2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "F:/skillbox-homework/Module - 18/quest2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
