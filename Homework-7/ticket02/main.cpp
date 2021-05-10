2acdb3b938a887dd0d3a31fb79ce71f5cafb0dbf
nosferati <artswanted@icloud.com>
2021-05-10 17:33:16 +0300

    ticket02

--- Homework-7/ticket02/main.cpp
+++ Homework-7/ticket02/main.cpp
@@ -8,7 +8,7 @@ int* revert (int* pArray, int* pArrayRevert){
 }
 
 int main() {
-    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int array[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int arrayRevert[10];
     int* pArray = array;
     int* pArrayRevert = arrayRevert;
