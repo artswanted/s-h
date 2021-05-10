c3f85b24eb46b7f26148ae1ae36c038e05b79c61
nosferati <artswanted@icloud.com>
2021-05-10 16:27:37 +0300

    ticket01

--- Homework-7/ticket01/main.cpp
+++ Homework-7/ticket01/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a = 20;
+    int b = 40;
+    int* pa = &a;
+    int* pb = &b;
+    std::cout << *pa << " " << *pb << std::endl;
+    std::swap(a, b);
+    std::cout << *pa << " " << *pb << std::endl;
     return 0;
 }
