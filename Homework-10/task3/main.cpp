4eca7ad3b0295e39e1684769773f8e1ac8cecad9
nosf <nosf@amatagames.com>
2021-07-28 19:36:12 +0300

    task3

--- Homework-10/task3/main.cpp
+++ Homework-10/task3/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <fstream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    std::ifstream fin("river.txt");
+    std::ofstream fout("basket.txt", std::ios::app);
+
+    if(!fout.is_open()) {
+        std::cout << "Basket is missing." << std::endl;
+    } else if(!fin.is_open()){
+        std::cout << "River is missing." << std::endl;
+    } else{
+        std::string fish;
+        std::cout << "Fish type: ";
+        std::cin >> fish;
+        int counter = 0;
+        while(!fin.eof()){
+            std::string tmp;
+            fin >> tmp;
+            if(tmp == fish){
+                fout << fish << std::endl;
+                ++counter;
+            }
+        }
+        std::cout << "amount of catch: " << counter << std::endl;
+    }
+    fin.close();
+    fout.close();
+
     return 0;
 }
