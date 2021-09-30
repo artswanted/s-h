acd0f5441726456b46cf610e83bde96e14f2ccf2
nosf <nosf@amatagames.com>
2021-09-30 22:56:01 +0300

    task2

--- Homework-12/task2/task2/task2.cpp
+++ Homework-12/task2/task2/task2.cpp
@@ -1,20 +1,29 @@
-﻿// task2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <map>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::map <std::string, std::string> registry;
+    std::string answer;
+    int count = 0;
+    while (true)
+    {
+        std::cout << "Please input a name" << std::endl;
+        std::cin >> answer;
+        if (answer == "next")
+        {
+            std::cout << registry.begin()->second << std::endl;
+            registry.erase(registry.begin());
+        }
+        else
+        {
+            //std::cout << answer + std::to_string(count) << std::endl;
+            //registry.insert(std::pair<std::string, std::string>(answer + std::to_string(count), answer));
+            registry.insert(std::make_pair(answer + std::to_string(count), answer));
+            count++;
+        }
+    }
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
