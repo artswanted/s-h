25290348dac9135a6444dee0f4586ef4407bd01f
nosf <nosf@amatagames.com>
2021-09-30 22:22:32 +0300

    task01

--- Homework-12/task1/main.cpp
+++ Homework-12/task1/main.cpp
@@ -3,21 +3,42 @@
 #include <string>
 
 int main() {
-    std::map <int, std::string> phoneNumbers;
+    std::map <std::string, std::string> nameToPhone;
 
     while (true) {
         std::string numberOwner;
-        int iPhoneNumber;
+        std::string sPhoneNumber;
         int answer;
-        std::cout << "Enter phone number (without +):";
-        std::cin >> iPhoneNumber;
-        std::cout << "Write Name:";
-        std::cin.ignore();
-        std::getline(std::cin, numberOwner);
-        phoneNumbers.insert(std::pair<int, std::string>(iPhoneNumber, numberOwner));
-        std::cout << "If you want add another number, write 1 or any other key for exit:";
+        std::cout << "1 - add new contact. 2 - found by surname:";
         std::cin >> answer;
-        if (answer != 1) break;
+
+        if (answer == 1) {
+            std::cout << "Enter phone number:";
+            std::cin >> sPhoneNumber;
+            std::cout << "Write surname:";
+            std::cin >> numberOwner;
+
+            if (sPhoneNumber != nameToPhone[numberOwner]) {
+                std::map <std::string, std::string>::iterator it = nameToPhone.find(numberOwner);
+                std::string sNewPhonePool = it->second + " " + sPhoneNumber;
+                nameToPhone.erase(it);
+                nameToPhone.insert(std::make_pair(numberOwner, sNewPhonePool));
+            }
+            else {
+                nameToPhone.insert(std::make_pair(numberOwner, sPhoneNumber));
+            }
+        }
+        else if (answer == 2) {
+            std::cout << "Write surname:";
+            std::cin >> numberOwner;
+            if (nameToPhone[numberOwner].size() > 0){
+                std::map <std::string, std::string>::iterator it = nameToPhone.find(numberOwner);
+                std::cout << it->first << ": " << it->second << std::endl;
+            } else std::cout << "Missing!";
+        }
+        else {
+            break;
+        }
     }
     std::cout << "Bye!" << std::endl;
     return 0;
