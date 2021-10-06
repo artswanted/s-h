12aeecc9b4f0e2991491c457d4416e116fcb67f6
nosf <nosf@amatagames.com>
2021-10-06 10:09:27 +0300

    task1

--- Homework-12/task1/main.cpp
+++ Homework-12/task1/main.cpp
@@ -2,44 +2,44 @@
 #include <map>
 #include <string>
 
+void include (std::map <std::string, std::string>& nMap, std::map <std::string, std::string>& oMap,std::string foo)
+{
+    int pos;
+    std::string firstPart, secondPart;
+    for (pos = 0; foo[pos] != ' '; ++pos);
+    for (int a = 0; a < pos; ++a) firstPart += foo[a];
+    for (int a = pos + 1; a < foo.length(); ++a) secondPart += foo[a];
+    nMap.insert(std::make_pair (firstPart, secondPart));
+    oMap.insert(std::make_pair(secondPart, firstPart));
+    if (firstPart != oMap[secondPart])
+    {
+        firstPart += " " + oMap [secondPart];
+        std::map <std::string, std::string> :: iterator it;
+        for (it = oMap.begin(); it->first != secondPart; ++ it);
+        oMap.erase(it);
+        oMap.insert(std::make_pair(secondPart, firstPart));
+    }
+}
+
 int main() {
     std::map <std::string, std::string> nameToPhone;
-
-    while (true) {
-        std::string numberOwner;
-        std::string sPhoneNumber;
-        int answer;
-        std::cout << "1 - add new contact. 2 - found by surname:";
-        std::cin >> answer;
-
-        if (answer == 1) {
-            std::cout << "Enter phone number:";
-            std::cin >> sPhoneNumber;
-            std::cout << "Write surname:";
-            std::cin >> numberOwner;
-
-            if (sPhoneNumber != nameToPhone[numberOwner]) {
-                std::map <std::string, std::string>::iterator it = nameToPhone.find(numberOwner);
-                std::string sNewPhonePool = it->second + " " + sPhoneNumber;
-                nameToPhone.erase(it);
-                nameToPhone.insert(std::make_pair(numberOwner, sNewPhonePool));
-            }
-            else {
-                nameToPhone.insert(std::make_pair(numberOwner, sPhoneNumber));
-            }
+    std::map <std::string, std::string> phoneToName;
+    std::string answer;
+    while (true)
+    {
+        std::cout << "Please enter a request" << std::endl;
+        std::getline(std::cin, answer);
+        if ((answer[0] >= '0' && answer[0] <= '9') &&
+            (answer[answer.length()-1] >= 'a' &&
+             answer[answer.length()-1] <= 'z'))
+        {
+            include (phoneToName, nameToPhone, answer);
         }
-        else if (answer == 2) {
-            std::cout << "Write surname:";
-            std::cin >> numberOwner;
-            if (nameToPhone[numberOwner].size() > 0){
-                std::map <std::string, std::string>::iterator it = nameToPhone.find(numberOwner);
-                std::cout << it->first << ": " << it->second << std::endl;
-            } else std::cout << "Missing!";
-        }
-        else {
-            break;
-        }
+        else if (answer[0] >= '0' && answer[0] <= '9')
+            std::cout << phoneToName [answer] << std::endl;
+        else if (answer[answer.length()-1] >= 'a' &&
+                 answer[answer.length()-1] <= 'z')
+            std::cout<< nameToPhone [answer]<<std::endl;
+        else std::cout << "Error input! Please try again" << std::endl;
     }
-    std::cout << "Bye!" << std::endl;
-    return 0;
 }
