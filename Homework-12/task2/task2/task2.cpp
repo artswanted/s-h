9e29259852fcb3f0adf5c661020791a175a8575f
nosf <nosf@amatagames.com>
2021-10-06 12:01:12 +0300

    task2

--- Homework-12/task2/task2/task2.cpp
+++ Homework-12/task2/task2/task2.cpp
@@ -2,7 +2,7 @@
 #include <map>
 #include <string>
 
-int main()
+/*int main()
 {
     std::map <int, std::string> registry;
     std::string answer;
@@ -27,6 +27,54 @@ int main()
             count++;
         }
     }
-}
+}*/
 
+int main()
+{
+    std::map <std::string, int> registry;
+    std::string answer;
+    int count = 1;
+
+    while (true)
+    {
+        std::cout << "Please input a name" << std::endl;
+        std::cin >> answer;
+        if (answer == "next")
+        {
+            std::cout << registry.begin()->first << std::endl;
+            std::map <std::string, int>::iterator it = registry.begin();
+            if (it->second > 1) {
+                int iTemp = it->second - 1;
+                std::string tempName = it->first;
+                registry.erase(registry.begin());
+                registry.insert(std::make_pair(tempName, iTemp));
+            }
+            else {
+                registry.erase(registry.begin());
+            }
+        }
+        else
+        {
+            if (registry.size() > 0){
+                bool temp = true;
+                for (std::map <std::string, int>::iterator it = registry.begin(); it != registry.end(); it++) {
+                    if (it->first == answer) {
+                        int iTemp = it->second + 1;
+                        registry.erase(answer);
+                        registry.insert(std::make_pair(answer, iTemp));
+                        temp = false;
+                        break;
+                    }
+                }
+                if (temp) {
+                    registry.insert(std::make_pair(answer, count));
+                }
+            }
+            else {
+                registry.insert(std::make_pair(answer, count));
+            }
+
+        }
+    }
+}
 
