9aead0e6d9f59cfcffff477a380d971e97d3cb96
nosf <nosf@amatagames.com>
2021-10-06 10:56:03 +0300

    task3

--- Homework-12/task3/task3/task3.cpp
+++ Homework-12/task3/task3/task3.cpp
@@ -1,20 +1,51 @@
-﻿// task3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <map>
+#include <string>
 
-#include <iostream>
+using namespace std;
+
+bool compareArrays(std::map <int, char>& arrayFirst, std::map <int, char>& arraySecond) {
+    int counter = 0;
+    if (arrayFirst.size() != arraySecond.size()) return false;
+    for (int i = 0; i < arrayFirst.size();) {
+        for (int b = 0; b < arraySecond.size(); ++b) {
+            if (arrayFirst[i] != arraySecond[b]) {
+                //cout << arrayFirst[i] << " VS " << arraySecond[b] << endl;
+                return false;
+            }
+            i++;
+        }
+    }
+    return true;
+}
+
+void arrayInsert(std::map <int, char>& textArray, string sUserText) {
+    for (int i = 0; i < sUserText.size(); i++) {
+        textArray.insert(make_pair (i, sUserText[i]));
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::map <int, char> word01;
+    std::map <int, char> word02;
+    bool check = true;
+    int count = 0;
+    string sUserText;
+
+    cout << "Input first word: ";
+    cin >> sUserText;
+    arrayInsert(word01, sUserText);
+
+    cout << "Input second word: ";
+    cin >> sUserText;
+    arrayInsert(word02, sUserText);
+
+    check = compareArrays(word01, word02);
+
+    if (check) {
+        std::cout << "Its anagram" << endl;
+    }
+    else cout << "its not anagram" << endl;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
