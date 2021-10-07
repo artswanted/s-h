7f2ae78614e759526614b5709c4b8280227ef6f2
nosf <nosf@amatagames.com>
2021-10-07 14:10:40 +0300

    task3

--- Homework-12/task3/task3/task3.cpp
+++ Homework-12/task3/task3/task3.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-bool compareArrays(std::map <int, char>& arrayFirst, std::map <int, char>& arraySecond) {
+bool compareArrays(std::map <char, char>& arrayFirst, std::map <char, char>& arraySecond) {
     int counter = 0;
     if (arrayFirst.size() != arraySecond.size()) return false;
     for (int i = 0; i < arrayFirst.size();) {
@@ -19,16 +19,16 @@ bool compareArrays(std::map <int, char>& arrayFirst, std::map <int, char>& array
     return true;
 }
 
-void arrayInsert(std::map <int, char>& textArray, string sUserText) {
+void arrayInsert(std::map <char, char>& textArray, string sUserText) {
     for (int i = 0; i < sUserText.size(); i++) {
-        textArray.insert(make_pair (i, sUserText[i]));
+        textArray.insert(make_pair (sUserText[i], sUserText[i]));
     }
 }
 
 int main()
 {
-    std::map <int, char> word01;
-    std::map <int, char> word02;
+    std::map <char, char> word01;
+    std::map <char, char> word02;
     bool check = true;
     int count = 0;
     string sUserText;
