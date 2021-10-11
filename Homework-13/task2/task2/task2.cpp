ce4e4ddf7f78cb831dfb3acc70e3d79258e5cf02
nosf <nosf@amatagames.com>
2021-10-11 12:34:59 +0300

    task2

---
+++ Homework-13/task2/task2/task2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+#define FILL(a)\
+for (int i = 0; i < 10; i++)\
+{\
+	std::cout << "Input passengers for wagon #" << i+1 << std::endl;\
+	std::cin >> a[i];\
+}\
+
+#define VAGONCHECKER(a)\
+for (int i=0; i<10; i++) \
+{\
+if (a[i] < 20) {\
+std::cout << "Wagon #" << i << " is empty." << std::endl;\
+}\
+if (a[i] > 20) {\
+std::cout << "Wagon #" << i << " is overloaded" << std::endl;\
+}\
+}\
+
+#define VAGONSUM(a)\
+int sum = 0;\
+for (int i = 0; i < 10; i++)\
+{\
+	sum += a[i];\
+}\
+std::cout << "Total passengers in all wagons are " << sum << std::endl;\
+
+int main()
+{
+	int a[10];
+	FILL(a);
+	VAGONCHECKER(a);
+
+}
