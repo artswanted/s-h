bc85c86765c960e4e1789081f5077b2aa2f371d9
nosf <nosf@amatagames.com>
2021-10-12 12:20:47 +0300

    task3

---
+++ Homework-13/task3/task3/task3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+//#define WINTER
+//#define SPRING
+//#define SUMMER
+#define FALL
+
+
+int main()
+{
+#ifdef WINTER
+    std::cout << "Its winter!";
+#endif
+
+#ifdef SPRING
+    std::cout << "Its spring!";
+#endif
+
+#ifdef SUMMER
+    std::cout << "Its summer!";
+#endif
+
+#ifdef FALL
+    std::cout << "Its autumn!";
+#endif
+
+}
+
