f5536aa5c2d79132510598780b2423bd808a931b
Dmytro <artswanted@icloud.com>
2021-10-19 15:53:49 +0300

    task3

--- Homework-14/task3/task3/task3.cpp
+++ Homework-14/task3/task3/task3.cpp
@@ -10,12 +10,21 @@ int main() {
     std::cin >> std::get_time(&time, "%M:%S");
     std::time_t t = std::time(nullptr) + time.tm_min * 60 + time.tm_sec;
 
+    std::time_t delta = t - std::time(nullptr);
+    int temp_data = delta % 60;
+
     while (true)
     {
-        system("CLS");
-        std::time_t delta = t - std::time(nullptr);
-        if (delta < 0) break;
-        std::cout << delta / 60 << ":" << delta % 60 << std::endl;
+        delta = t - std::time(nullptr);
+        if (delta <= 0) break;
+
+        if (temp_data != delta % 60) {
+            system("CLS");
+            std::cout << delta / 60 << ":" << delta % 60 << std::endl;
+            temp_data = delta % 60;
+        }
+        
+        
     }
     system("CLS");
     std::cout << "BEEP! BEEP!\n";
