0a28ee061240d7b8645e09511db23d5a8f26f836
nosf <nosf@amatagames.com>
2021-10-22 21:40:51 +0300

    task1

--- Homework-15/task1/source/main.cpp
+++ Homework-15/task1/source/main.cpp
@@ -2,35 +2,42 @@
 
 using namespace std;
 
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "EndlessLoop"
 int main() {
     POINT begin, end, temp;
     string choice;
-
+    bool start = true;
     print_message(MESSAGE::WELCOME);
-    cin >> choice;
 
-    if (choice == "scalpel"s){
-        scalpel_start(begin, end);
+    while (start){
+        cin >> choice;
 
-        while (choice != "suture"s){
-            print_message(NEXT_STEP);
-            cin >> choice;
-            if (choice == "scalpel"s){
-                print_message(ALREADY_STARTED);
-            } else if (choice == "hemostat"s) {
-                hemostat_start(begin, end, temp);
-            } else if (choice == "tweezers"s){
-                tweezers_start(begin, end, temp);
-            } else if (choice == "suture"s){
-                suture_start (begin, end);
-            } else {
-                print_message(WRONG_INPUT);
+        if (choice == "scalpel"s) {
+            scalpel_start(begin, end);
+
+            while (choice != "suture"s) {
+                print_message(NEXT_STEP);
+                cin >> choice;
+                if (choice == "scalpel"s) {
+                    print_message(ALREADY_STARTED);
+                } else if (choice == "hemostat"s) {
+                    hemostat_start(begin, end, temp);
+                } else if (choice == "tweezers"s) {
+                    tweezers_start(begin, end, temp);
+                } else if (choice == "suture"s) {
+                    suture_start(begin, end);
+                    start = false;
+                } else {
+                    print_message(WRONG_INPUT);
+                }
             }
-        }
 
-    } else {
-        print_message(WRONG_START);
+        } else {
+            print_message(WRONG_START);
+        }
     }
 
     return 0;
 }
+
