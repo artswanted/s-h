78cbcc2de0bdd0e916a98fd0938dc8bd32c4b52f
nosf <nosf@amatagames.com>
2021-10-22 17:27:55 +0300

    task1

---
+++ Homework-15/task1/include/operation.h
@@ -0,0 +1,8 @@
+//
+// Created by dmytr on 22.10.2021.
+//
+
+#ifndef SIMULATOR_OPERATION_H
+#define SIMULATOR_OPERATION_H
+
+#endif //SIMULATOR_OPERATION_H
---
+++ Homework-15/task1/source/operation.cpp
@@ -0,0 +1,4 @@
+//
+// Created by dmytr on 22.10.2021.
+//
+
