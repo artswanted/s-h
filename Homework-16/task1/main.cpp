8449a73a4c6b61d335aaeb61388056ed507bc44c
nosf <nosf@amatagames.com>
2021-11-27 18:47:08 +0200

    task1

---
+++ Homework-16/task1/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+
+class Track {
+private:
+    std::string musicList[3][5] = {{"Blinding Lights", "The Twist", "Mack the Knife",
+                                     "How Do I Live", "Macarena"},
+                                   {"29 November 2019", "19 September 1960", "23 July 1970", "19 September 2000", "23 July 1971"},
+                                   {"202", "147", "252", "202", "412"}};
+public:
+    std::string getNextTrack()
+    {
+        return musicList[1][1];
+    }
+
+    bool trackChecker(std::string trackName){
+        return true;
+    }
+};
+
+class WinampMediaPlater{
+private:
+    Track Track_t;
+    int currentTrack = 0;
+public:
+    void play(std::string trackName){
+        if (Track_t.trackChecker(trackName)){
+
+            std::cout << Track_t.getNextTrack();
+        }
+    }
+
+};
+
+int main() {
+    WinampMediaPlater WinPlayer;
+    std::string command, trackName;
+    do {
+        std::cout << "Input command:";
+        std::cin >> command;
+        if ("play" == command) {
+            std::cout << "Input trask name:";
+            std::cin >> trackName;
+            WinPlayer.play(trackName);
+        } else if ("pause" == command){
+
+        } else if ("next" == command){
+
+        } else if ("stop" == command){
+
+        } else if ("exit" == command){
+            break;
+        }else{
+            std::cout << "Wrong input!";
+        }
+    } while (true);
+    return 0;
+}
