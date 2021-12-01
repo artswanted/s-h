6b7c37fcee72f70551b005433e081bcebc407bc7
nosf <nosf@amatagames.com>
2021-12-01 19:29:19 +0200

    task1

--- Homework-16/task1/main.cpp
+++ Homework-16/task1/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 
 class Track {
 private:
@@ -7,32 +8,94 @@ private:
                                      "How Do I Live", "Macarena"},
                                    {"29 November 2019", "19 September 1960", "23 July 1970", "19 September 2000", "23 July 1971"},
                                    {"202", "147", "252", "202", "412"}};
+    int playedTrack = 0;
+    bool trackPause = false;
 public:
     std::string getNextTrack()
     {
-        return musicList[1][1];
+        std::srand(time(0));
+        int randTrackNo = rand() % 4;
+        playedTrack = randTrackNo;
+        return musicList[0][randTrackNo];
     }
 
-    std::string trackChecker(std::string trackName){
-        return;
+    std::string playTrack(std::string trackName){
+        if (!trackPause){
+            bool trackFound = false;
+            for (int i = 0; i < 5; i++){
+                std::string soundName = musicList[0][i];
+                if (trackName.size() == soundName.size()){
+                    for (int j = 0; j < trackName.size(); j++){
+                        if (trackName[j] != soundName[j]){
+                            trackFound = false;
+                            break;
+                        } else {
+                            trackFound = true;
+                        }
+                    }
+                    if (trackFound){
+                        playedTrack = i;
+                        return soundName;
+                    }
+                }
+            }
+            return "null";
+        }
+        else {
+            trackPause = false;
+            return musicList[0][playedTrack];
+        }
+
+    }
+
+    void trackDate(){
+        std::cout << musicList[1][playedTrack] << " created date." << std::endl;
+    }
+
+    void trackTime(){
+        std::cout << musicList[2][playedTrack] << " total track time." << std::endl;
+    }
+
+    void pauseTrack(){
+        if (!trackPause){
+            trackPause = true;
+            std::cout << "Pause!";
+        }
     }
 };
 
 class WinampMediaPlater{
 private:
     Track Track_t;
-    int currentTrack = 0;
+    bool playStatus = false;
 public:
     void play(std::string trackName){
-        if (currentTrack == 0){
-            std::string name = Track_t.trackChecker(trackName);
-            if (name != "null"){
-                currentTrack = 1;
-                std::cout << name;
-            } else if (name == "null"){
-                std::cout << "Track is nor found!";
+        if (!playStatus){
+            std::string result = Track_t.playTrack(trackName);
+            std::cout << (result == "null" ? "Wrong track name!" : result + " is playing.\n");
+            if (result != "null"){
+                Track_t.trackTime();
+                Track_t.trackDate();
+                playStatus = true;
             }
         }
+    }
+
+    void pause (){
+        Track_t.pauseTrack();
+    }
+
+    void stop(){
+        if (playStatus){
+            std::cout << "Stopped!";
+            playStatus = false;
+        }
+    }
+
+    void next(){
+        std::cout << Track_t.getNextTrack() << std::endl;
+        Track_t.trackTime();
+        Track_t.trackDate();
     }
 
 };
@@ -45,14 +108,15 @@ int main() {
         std::cin >> command;
         if ("play" == command) {
             std::cout << "Input trask name:";
-            std::cin >> trackName;
+            std::cin.ignore();
+            std::getline(std::cin, trackName) ;
             WinPlayer.play(trackName);
         } else if ("pause" == command){
-
+            WinPlayer.pause();
         } else if ("next" == command){
-
+            WinPlayer.next();
         } else if ("stop" == command){
-
+            WinPlayer.stop();
         } else if ("exit" == command){
             break;
         }else{
