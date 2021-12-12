356e47f952665dd5c370e3eb9e271f99442b8c8a
Dmytro <artswanted@icloud.com>
2021-12-12 16:20:44 +0200

    task3

--- Homework-16/task3/main.cpp
+++ Homework-16/task3/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-class Monitor
+class Display
 {
 public:
     int x;
@@ -9,30 +9,32 @@ public:
     int height;
 };
 
-Monitor myMonitor;
+
 
 class Command
 {
+private:
+    Display myDisplay;
 public:
     void move ()
     {
         int newX, newY;
         std::cout << "Please enter new beginning:" << std::endl;
         std::cin >> newX >> newY;
-        while ((newX + myMonitor.width) > 80 && ((newY + myMonitor.height) > 50))
+        while ((newX + myDisplay.width) > 80 && ((newY + myDisplay.height) > 50))
         {
             std::cout << "Incorrect input, please try again" << std::endl;
             std::cin >> newX >> newY;
         }
-        myMonitor.x = newX;
-        myMonitor.y = newY;
+        myDisplay.x = newX;
+        myDisplay.y = newY;
     };
     void resize ()
     {
         int newWidth, newHeight;
         std::cout << "Please enter new width and new heught of the window" << std::endl;
         std::cin >> newWidth >> newHeight;
-        while ((myMonitor.x+newWidth) > 80 && (myMonitor.y + newHeight) > 50)
+        while ((myDisplay.x+newWidth) > 80 && (myDisplay.y + newHeight) > 50)
         {
             std::cout << "Incorrect input, please try again" << std::endl;
             std::cin >> newWidth >> newHeight;
@@ -44,7 +46,7 @@ public:
         {
             for (int j = 0; j <=80; j++)
             {
-                if (i >= myMonitor.y && i <= (myMonitor.y+myMonitor.height) && j>=myMonitor.x && j<=(myMonitor.x + myMonitor.width)) std::cout << "1";
+                if (i >= myDisplay.y && i <= (myDisplay.y+myDisplay.height) && j>=myDisplay.x && j<=(myDisplay.x + myDisplay.width)) std::cout << "1";
                 else std::cout << "0";
             }
             std::cout << std::endl;
@@ -53,22 +55,23 @@ public:
 };
 
 int main() {
+    Display myDisplay;
     std::cout << "Please enter initial parameters of Window" << std::endl;
-    std::cin >> myMonitor.x >> myMonitor.y >> myMonitor.width >> myMonitor.height;
-    while ((myMonitor.x+myMonitor.width) > 80 && (myMonitor.y + myMonitor.height) > 50)
+    std::cin >> myDisplay.x >> myDisplay.y >> myDisplay.width >> myDisplay.height;
+    while ((myDisplay.x+myDisplay.width) > 80 && (myDisplay.y + myDisplay.height) > 50)
     {
         std::cout << "Incorrect entry, please try again" << std::endl;
-        std::cin >> myMonitor.x >> myMonitor.y >> myMonitor.width >> myMonitor.height;
+        std::cin >> myDisplay.x >> myDisplay.y >> myDisplay.width >> myDisplay.height;
     }
     int answer;
     Command myCommand;
-    while (answer != 3)
+    do
     {
         std::cout << "Please choose a command" << std:: endl;
         std::cout << "0. move Window" << std::endl;
         std::cout << "1. resize Window" << std::endl;
-        std::cout << "2.display Window" << std::endl;
-        std::cout << "3.exit" << std::endl;
+        std::cout << "2. display Window" << std::endl;
+        std::cout << "3. exit" << std::endl;
         std::cin >> answer;
         switch (answer)
         {
@@ -86,6 +89,6 @@ int main() {
             default:
                 std::cout << "! Incorrect command. Try again !";
                 break;
-        }
+        }while (true);
     }
 }
