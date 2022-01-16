128b25409b536fe3d51b1d2173b6d0b1741219be
Dmytro <77779585+artswanted@users.noreply.github.com>
2022-01-16 11:57:33 +0200

    task1

---
+++ Homework-17/task1/main.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <vector>
+
+class Bigstick
+{
+  public:
+  std::vector <std::string> names;
+
+  void amountGeneration ()
+  {
+    names.resize(std::rand() % 2 + 2);
+  };
+  void placement()
+  {
+    for (int i = 0; i < names.size(); i++)
+    {
+      std::cout << "middlestick " << i + 1 << " settlement" <<std::endl;
+      std::cout << " Please enter a name of Elf" << std::endl;
+      std::cin >> names[i];
+    }
+  };
+  void foundNeighborsStick (std::string foo)
+  {
+    for (int i = 0; i < names.size(); i++)
+    {
+      if (foo == names[i])
+      {
+        std::cout << "Neighbors of " << foo << " are:" << std::endl;
+        for (int j = 0; j < names.size(); j++)
+        {
+          if (j != i) std::cout << names[j] << std::endl;
+        }
+      }
+    }
+  }
+};
+
+
+class Tree
+{
+  public:
+  std::vector <class Bigstick> tree;
+
+  void amountOfBigSticks ()
+  {
+    tree.resize(std::rand() % 3 + 3);
+  }
+
+  void treePlacement ()
+  {
+    for (int i=0; i < tree.size(); i ++)
+    {
+      std::cout << "Bigstick " << i + 1 << " settlement" << std::endl;
+      tree[i].amountGeneration();
+      tree[i].placement();
+    }
+  };
+  void foundNeighborsTree (std::string foo)
+  {
+    for (int i = 0; i < tree.size(); i++)
+    {
+      tree[i].foundNeighborsStick(foo);
+    }
+  }
+};
+
+class Village
+{
+  public:
+  std::vector <class Tree> village;
+  Village () : village(5){}
+
+  void settlement()
+  {
+    std::cout << "Village Settlement" << std::endl;
+    for (int i = 0; i < village.size(); i++)
+    {
+      std::cout << "Tree " << i + 1 << " settlement" << std::endl;
+      village[i].amountOfBigSticks();
+      village[i].treePlacement();
+    }
+  }
+  void foundNeighborsVillage (std::string foo)
+  {
+    for (int i = 0; i < village.size(); i++)
+    {
+      village[i].foundNeighborsTree(foo);
+    }
+  }
+
+};
+
+
+
+int main() {
+  Village myVillage;
+  myVillage.settlement();
+  std::cout << "Who are we looking for?" << std:: endl;
+  std::string answer;
+  std::cin >> answer;
+  myVillage.foundNeighborsVillage(answer);
+}
