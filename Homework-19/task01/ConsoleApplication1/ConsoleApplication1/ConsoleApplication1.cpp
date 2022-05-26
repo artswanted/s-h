3727e6355e05e1581d004c400a184468fa90975d
nosf <nosf@amatagames.com>
2022-05-26 22:41:55 +0300

    task1

---
+++ Homework-19/task01/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+
+class Animal
+{
+public:
+    virtual void voice() = 0;
+};
+
+class Dog : public Animal
+{
+public:
+    virtual void voice()
+    {
+        std::string voice = "Bark";
+    }
+};
+
+class Cat : public Animal
+{
+public:
+    virtual void voice()
+    {
+        std::string voice = "Bark";
+    }
+};
+
+void meeting(Animal* a, Animal* b)
+{
+    if (a->voice() == "Bark")
+}
+
+
+
+int main() {
+    Animal* a = new Dog();
+    Animal* b = new Cat();
+    meeting(a, b);
+    meeting(a, a);
+    meeting(b, b);
+    meeting(b, a);
+}
